75f1a134bb3aa3044028afef9d1dbcb72aa1e8eb
Daniel Cazzulino <daniel@cazzulino.com>
2014-08-11 15:58:21 -0300

    Fixed issue preserving labels and assignees

--- Tests/AutoUpdateTests.cs
+++ Tests/AutoUpdateTests.cs
@@ -1,204 +1,204 @@
 ﻿namespace Tests
 {
-	using Newtonsoft.Json.Linq;
-	using OctoHook;
-	using Octokit;
-	using Octokit.Events;
-	using Octokit.Internal;
-	using System;
-	using System.Collections.Generic;
-	using System.IO;
-	using System.Linq;
-	using System.Reflection;
-	using System.Text;
-	using System.Text.RegularExpressions;
-	using System.Threading.Tasks;
-	using Xunit;
+    using Newtonsoft.Json.Linq;
+    using OctoHook;
+    using Octokit;
+    using Octokit.Events;
+    using Octokit.Internal;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Xunit;
 
-	public class AutoUpdateTests
-	{
-		static readonly Credentials credentials = new Credentials(File.ReadAllText(@"..\..\Token").Trim());
+    public class AutoUpdateTests
+    {
+        static readonly Credentials credentials = new Credentials(File.ReadAllText(@"..\..\Token").Trim());
 
-		[Fact]
-		public async Task when_processing_issue_with_lower_case_label_then_automatically_adds_labels()
-		{
-			var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
-			var repository = await github.Repository.Get("kzu", "sandbox");
-			var user = await github.User.Current();
+        [Fact]
+        public async Task when_processing_issue_with_lower_case_label_then_automatically_adds_labels()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
+            var repository = await github.Repository.Get("kzu", "sandbox");
+            var user = await github.User.Current();
 
-			var issue = await github.Issue.Create(
-				"kzu", "sandbox", new NewIssue("Auto-labeling to stories ~story"));
+            var issue = await github.Issue.Create(
+                "kzu", "sandbox", new NewIssue("Auto-labeling to stories ~story"));
 
-			var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
+            var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
 
-			await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
-			{
-				Action = IssuesEvent.IssueAction.Opened,
-				Issue = issue,
-				Repository = repository,
-				Sender = user,
-			});
+            await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
+            {
+                Action = IssuesEvent.IssueAction.Opened,
+                Issue = issue,
+                Repository = repository,
+                Sender = user,
+            });
 
-			var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
+            var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
 
-			Assert.Equal("Auto-labeling to stories", updated.Title);
-			Assert.True(updated.Labels.Any(l => l.Name == "Story"));
+            Assert.Equal("Auto-labeling to stories", updated.Title);
+            Assert.True(updated.Labels.Any(l => l.Name == "Story"));
 
-			await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
-		}
+            await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
+        }
 
-		[Fact]
-		public async Task when_processing_issue_with_declared_mid_title_then_does_not_apply_label()
-		{
-			var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
-			var repository = await github.Repository.Get("kzu", "sandbox");
-			var user = await github.User.Current();
+        [Fact]
+        public async Task when_processing_issue_with_declared_mid_title_then_does_not_apply_label()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
+            var repository = await github.Repository.Get("kzu", "sandbox");
+            var user = await github.User.Current();
 
-			var issue = await github.Issue.Create(
-				"kzu", "sandbox", new NewIssue("Auto-labeling to ~foo in the middle doesn't work"));
+            var issue = await github.Issue.Create(
+                "kzu", "sandbox", new NewIssue("Auto-labeling to ~foo in the middle doesn't work"));
 
-			var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
+            var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
 
-			await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
-			{
-				Action = IssuesEvent.IssueAction.Opened,
-				Issue = issue,
-				Repository = repository,
-				Sender = user,
-			});
+            await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
+            {
+                Action = IssuesEvent.IssueAction.Opened,
+                Issue = issue,
+                Repository = repository,
+                Sender = user,
+            });
 
-			var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
+            var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
 
-			Assert.Equal("Auto-labeling to ~foo in the middle doesn't work", updated.Title);
-			Assert.False(updated.Labels.Any(l => l.Name == "foo"));
+            Assert.Equal("Auto-labeling to ~foo in the middle doesn't work", updated.Title);
+            Assert.False(updated.Labels.Any(l => l.Name == "foo"));
 
-			await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
-		}
+            await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
+        }
 
-		[Fact]
-		public async Task when_processing_issue_with_declared_plus_label_then_applies_it_with_plus()
-		{
-			var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
-			var repository = await github.Repository.Get("kzu", "sandbox");
-			var user = await github.User.Current();
+        [Fact]
+        public async Task when_processing_issue_with_declared_plus_label_then_applies_it_with_plus()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
+            var repository = await github.Repository.Get("kzu", "sandbox");
+            var user = await github.User.Current();
 
-			var issue = await github.Issue.Create(
-				"kzu", "sandbox", new NewIssue("Auto-labeling to +doc"));
+            var issue = await github.Issue.Create(
+                "kzu", "sandbox", new NewIssue("Auto-labeling to +doc"));
 
-			var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
+            var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
 
-			await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
-			{
-				Action = IssuesEvent.IssueAction.Opened,
-				Issue = issue,
-				Repository = repository,
-				Sender = user,
-			});
+            await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
+            {
+                Action = IssuesEvent.IssueAction.Opened,
+                Issue = issue,
+                Repository = repository,
+                Sender = user,
+            });
 
-			var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
+            var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
 
-			Assert.Equal("Auto-labeling to", updated.Title);
-			Assert.True(updated.Labels.Any(l => l.Name == "+Doc"));
+            Assert.Equal("Auto-labeling to", updated.Title);
+            Assert.True(updated.Labels.Any(l => l.Name == "+Doc"));
 
-			await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
-		}
+            await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
+        }
 
-		[Fact]
-		public async Task when_processing_issue_with_declared_minus_label_then_applies_it_with_plus()
-		{
-			var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
-			var repository = await github.Repository.Get("kzu", "sandbox");
-			var user = await github.User.Current();
+        [Fact]
+        public async Task when_processing_issue_with_declared_minus_label_then_applies_it_with_minus()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
+            var repository = await github.Repository.Get("kzu", "sandbox");
+            var user = await github.User.Current();
 
-			var issue = await github.Issue.Create(
-				"kzu", "sandbox", new NewIssue("Auto-labeling to -qa"));
+            var issue = await github.Issue.Create(
+                "kzu", "sandbox", new NewIssue("Auto-labeling to -qa"));
 
-			var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
+            var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
 
-			await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
-			{
-				Action = IssuesEvent.IssueAction.Opened,
-				Issue = issue,
-				Repository = repository,
-				Sender = user,
-			});
+            await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
+            {
+                Action = IssuesEvent.IssueAction.Opened,
+                Issue = issue,
+                Repository = repository,
+                Sender = user,
+            });
 
-			var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
+            var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
 
-			Assert.Equal("Auto-labeling to", updated.Title);
-			Assert.True(updated.Labels.Any(l => l.Name == "-QA"));
+            Assert.Equal("Auto-labeling to", updated.Title);
+            Assert.True(updated.Labels.Any(l => l.Name == "-QA"));
 
-			await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
-		}
+            await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
+        }
 
-		[Fact]
-		public async Task when_processing_issue_with_undeclared_plus_label_then_applies_it_without_plus()
-		{
-			var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
+        [Fact]
+        public async Task when_processing_issue_with_undeclared_plus_label_then_applies_it_without_plus()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
 
-			var repository = await github.Repository.Get("kzu", "sandbox");
-			var user = await github.User.Current();
+            var repository = await github.Repository.Get("kzu", "sandbox");
+            var user = await github.User.Current();
 
-			var issue = await github.Issue.Create(
-				"kzu", "sandbox", new NewIssue("Auto-labeling to +foo"));
+            var issue = await github.Issue.Create(
+                "kzu", "sandbox", new NewIssue("Auto-labeling to +foo"));
 
-			var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
+            var labeler = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoLabel(github) });
 
-			try
-			{
-				await github.Issue.Labels.Delete("kzu", "sandbox", "foo");
-			}
-			catch { }
+            try
+            {
+                await github.Issue.Labels.Delete("kzu", "sandbox", "foo");
+            }
+            catch { }
 
-			await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
-			{
-				Action = IssuesEvent.IssueAction.Opened,
-				Issue = issue,
-				Repository = repository,
-				Sender = user,
-			});
+            await labeler.ProcessAsync(new Octokit.Events.IssuesEvent
+            {
+                Action = IssuesEvent.IssueAction.Opened,
+                Issue = issue,
+                Repository = repository,
+                Sender = user,
+            });
 
-			var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
+            var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
 
-			Assert.Equal("Auto-labeling to", updated.Title);
-			Assert.True(updated.Labels.Any(l => l.Name == "foo"));
+            Assert.Equal("Auto-labeling to", updated.Title);
+            Assert.True(updated.Labels.Any(l => l.Name == "foo"));
 
-			await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
+            await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
 
-			try
-			{
-				await github.Issue.Labels.Delete("kzu", "sandbox", "foo");
-			}
-			catch { }
-		}
+            try
+            {
+                await github.Issue.Labels.Delete("kzu", "sandbox", "foo");
+            }
+            catch { }
+        }
 
-		[Fact]
-		public async Task when_processing_issue_with_colon_me_then_assigns_to_me()
-		{
-			var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
+        [Fact]
+        public async Task when_processing_issue_with_colon_me_then_assigns_to_me()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("kzu-client"), new InMemoryCredentialStore(credentials));
 
-			var repository = await github.Repository.Get("kzu", "sandbox");
-			var user = await github.User.Current();
+            var repository = await github.Repository.Get("kzu", "sandbox");
+            var user = await github.User.Current();
 
-			var issue = await github.Issue.Create(
-				"kzu", "sandbox", new NewIssue("Auto-assigning to :me"));
+            var issue = await github.Issue.Create(
+                "kzu", "sandbox", new NewIssue("Auto-assigning to :me"));
 
-			var updater = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoAssign() });
+            var updater = new OctoIssuerJob(github, new IOctoIssuer[] { new AutoAssign() });
 
-			await updater.ProcessAsync(new Octokit.Events.IssuesEvent
-			{
-				Action = IssuesEvent.IssueAction.Opened,
-				Issue = issue,
-				Repository = repository,
-				Sender = user,
-			});
+            await updater.ProcessAsync(new Octokit.Events.IssuesEvent
+            {
+                Action = IssuesEvent.IssueAction.Opened,
+                Issue = issue,
+                Repository = repository,
+                Sender = user,
+            });
 
-			var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
+            var updated = await github.Issue.Get("kzu", "sandbox", issue.Number);
 
-			Assert.Equal("Auto-assigning to", updated.Title);
-			Assert.Equal("kzu", updated.Assignee.Login);
+            Assert.Equal("Auto-assigning to", updated.Title);
+            Assert.Equal("kzu", updated.Assignee.Login);
 
-			await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
-		}
-	}
+            await github.Issue.Update("kzu", "sandbox", issue.Number, new IssueUpdate { State = ItemState.Closed });
+        }
+    }
 }
--- Web/OctoIssuerJob.cs
+++ Web/OctoIssuerJob.cs
@@ -1,120 +1,120 @@
 ﻿namespace OctoHook
 {
-	using Newtonsoft.Json;
-	using OctoHook.CommonComposition;
-	using OctoHook.Diagnostics;
-	using Octokit;
-	using Octokit.Events;
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text.RegularExpressions;
-	using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using OctoHook.CommonComposition;
+    using OctoHook.Diagnostics;
+    using Octokit;
+    using Octokit.Events;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
 
-	/// <summary>
-	/// Processes available <see cref="IOctoIssuer"/> components for 
-	/// the received issue webhook event.
-	/// </summary>
-	/// <devdoc>
-	/// See <see cref="IOctoIssuer"/> on why we need this abstraction.
-	/// We keep applying updaters as long as their <see cref="IOctoIssuer.Process"/>
-	/// returns true, telling us that a change might have been applied to the issue 
-	/// title.
-	/// </devdoc>
-	[Component]
-	public class OctoIssuerJob : IOctoJob<IssuesEvent>
-	{
-		static readonly ITracer tracer = Tracer.Get<OctoIssuerJob>();
+    /// <summary>
+    /// Processes available <see cref="IOctoIssuer"/> components for 
+    /// the received issue webhook event.
+    /// </summary>
+    /// <devdoc>
+    /// See <see cref="IOctoIssuer"/> on why we need this abstraction.
+    /// We keep applying updaters as long as their <see cref="IOctoIssuer.Process"/>
+    /// returns true, telling us that a change might have been applied to the issue 
+    /// title.
+    /// </devdoc>
+    [Component]
+    public class OctoIssuerJob : IOctoJob<IssuesEvent>
+    {
+        static readonly ITracer tracer = Tracer.Get<OctoIssuerJob>();
 
-		IGitHubClient github;
-		IEnumerable<IOctoIssuer> issuers;
+        IGitHubClient github;
+        IEnumerable<IOctoIssuer> issuers;
 
-		public OctoIssuerJob(IGitHubClient github, IEnumerable<IOctoIssuer> issuers)
-		{
-			this.github = github;
-			this.issuers = issuers.Select(i => new TracingOctoIssuer(tracer, i)).ToList();
-		}
+        public OctoIssuerJob(IGitHubClient github, IEnumerable<IOctoIssuer> issuers)
+        {
+            this.github = github;
+            this.issuers = issuers.Select(i => new TracingOctoIssuer(tracer, i)).ToList();
+        }
 
-		public async Task ProcessAsync(IssuesEvent issue)
-		{
-			tracer.Verbose("Running IOctoIssuer components for https://github.com/{0}/{1}/issues/{2}",
-				issue.Repository.Owner.Login,
-				issue.Repository.Name,
-				issue.Issue.Number);
+        public async Task ProcessAsync(IssuesEvent issue)
+        {
+            tracer.Verbose("Running IOctoIssuer components for https://github.com/{0}/{1}/issues/{2}",
+                issue.Repository.Owner.Login,
+                issue.Repository.Name,
+                issue.Issue.Number);
 
-			var update = new IssueUpdate { Title = issue.Issue.Title.Trim() };
+            var update = new IssueUpdate { Title = issue.Issue.Title.Trim() };
             if (issue.Issue.Assignee != null)
                 update.Assignee = issue.Issue.Assignee.Login;
             if (issue.Issue.Milestone != null)
                 update.Milestone = issue.Issue.Milestone.Number;
-            if (issue.Issue.Labels.Any())
+            if (issue.Issue.Labels != null)
                 update.Labels.AddRange(issue.Issue.Labels.Select(l => l.Name));
 
-			var updated = false;
+            var updated = false;
 
-			while (issuers.Any(updater => updater.Process(issue, update)))
-			{
-				update.Title = update.Title.Trim();
-				updated = true;
-			}
+            while (issuers.Any(updater => updater.Process(issue, update)))
+            {
+                update.Title = update.Title.Trim();
+                updated = true;
+            }
 
-			if (updated)
-			{
-                var labels = update.Labels.Distinct();
+            if (updated)
+            {
+                var labels = update.Labels.Distinct().ToArray();
                 update.Labels.Clear();
                 update.Labels.AddRange(labels);
 
-				await github.Issue.Update(issue.Repository.Owner.Login, issue.Repository.Name, issue.Issue.Number, update);
+                await github.Issue.Update(issue.Repository.Owner.Login, issue.Repository.Name, issue.Issue.Number, update);
 
-				var updates = new List<string>();
-				if (update.Labels.Any())
-					updates.Add(" labels [" + string.Join(", ", update.Labels) + "]");
-				if (!string.IsNullOrEmpty(update.Assignee))
-					updates.Add(" assignee '" + update.Assignee + "'");
-				if (!string.IsNullOrEmpty(update.Body))
-					updates.Add(" body '" + update.Body + "'");
+                var updates = new List<string>();
+                if (update.Labels.Any())
+                    updates.Add(" labels [" + string.Join(", ", update.Labels) + "]");
+                if (!string.IsNullOrEmpty(update.Assignee))
+                    updates.Add(" assignee '" + update.Assignee + "'");
+                if (!string.IsNullOrEmpty(update.Body))
+                    updates.Add(" body '" + update.Body + "'");
 
-				tracer.Info(@"Updated issue {0}/{1}#{2} with {3}.",
-					issue.Repository.Owner.Login,
-					issue.Repository.Name,
-					issue.Issue.Number,
-					string.Join(", ", updates));
-			}
-			else
-			{
-				tracer.Verbose(@"Skipped issue {0}/{1}#{2} since it had no applicable auto-updates.",
-					issue.Repository.Owner.Login,
-					issue.Repository.Name,
-					issue.Issue.Number);
-			}
-		}
+                tracer.Info(@"Updated issue {0}/{1}#{2} with {3}.",
+                    issue.Repository.Owner.Login,
+                    issue.Repository.Name,
+                    issue.Issue.Number,
+                    string.Join(", ", updates));
+            }
+            else
+            {
+                tracer.Verbose(@"Skipped issue {0}/{1}#{2} since it had no applicable auto-updates.",
+                    issue.Repository.Owner.Login,
+                    issue.Repository.Name,
+                    issue.Issue.Number);
+            }
+        }
 
-		private class TracingOctoIssuer : IOctoIssuer
-		{
-			ITracer tracer;
-			IOctoIssuer issuer;
-			string name;
+        private class TracingOctoIssuer : IOctoIssuer
+        {
+            ITracer tracer;
+            IOctoIssuer issuer;
+            string name;
 
-			public TracingOctoIssuer(ITracer tracer, IOctoIssuer issuer)
-			{
-				this.tracer = tracer;
-				this.issuer = issuer;
-				this.name = issuer.GetType().Name;
-			}
+            public TracingOctoIssuer(ITracer tracer, IOctoIssuer issuer)
+            {
+                this.tracer = tracer;
+                this.issuer = issuer;
+                this.name = issuer.GetType().Name;
+            }
 
-			public bool Process(IssuesEvent issue, IssueUpdate update)
-			{
-				tracer.Verbose("Processing with {0}", name);
+            public bool Process(IssuesEvent issue, IssueUpdate update)
+            {
+                tracer.Verbose("Processing with {0}", name);
 
-				var result = issuer.Process(issue, update);
-				
-				if (result)
-					tracer.Info("{0} updated issue. All components will be run again to apply further updates to it.", name);
-				else
-					tracer.Verbose("No changes performed by {0}", name);
+                var result = issuer.Process(issue, update);
 
-				return result;
-			}
-		}
-	}
+                if (result)
+                    tracer.Info("{0} updated issue. All components will be run again to apply further updates to it.", name);
+                else
+                    tracer.Verbose("No changes performed by {0}", name);
+
+                return result;
+            }
+        }
+    }
 }

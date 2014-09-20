798cf1ae0d50a088704d6ae3f9e16769560584c5
Daniel Cazzulino <daniel@cazzulino.com>
2014-09-20 19:53:00 -0300

    Only accept +label, so we can reserve the other chars

--- OctoHook.AutoLabel/Properties/AssemblyInfo.cs
+++ OctoHook.AutoLabel/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("OctoHook.AutoLabel")]
-[assembly: AssemblyDescription("Automatically apply labels to issues with title formatted with one or more '+[label]', '~[label]' or '-[label]'.")]
+[assembly: AssemblyDescription("Automatically apply labels to issues with title formatted with one or more '+[label]'.")]
 [assembly: AssemblyMetadata("projectUrl", "https://github.com/kzu/OctoHook/wiki/AutoLabel")]
